98c79c8085c921d8dfa339a4b1bd91809568ec6f
WENNSHOW <109863882+WENNSHOW@users.noreply.github.com>
2022-07-23 19:30:29 +0300

    Update задача 1.py

--- задача 1.py
+++ задача 1.py
@@ -1,12 +1,12 @@
 # 1 способ
-f=open('17.txt').readlines()
+f=open('17.1.txt').readlines()
 sp=[int(x) for x in f]
 sp1=[x for x in sp if 999<x<10000]
 sp2=[x for x in sp1 if int(str(x)[1])%2!=0]
 print(len(sp2),sum(sp2))
 
 # 2 способ
-f=open('17.txt').readlines()
+f=open('17.1.txt').readlines()
 sp=[int(x) for x in f]
 sp1=[]
 for i in range(len(sp)):
